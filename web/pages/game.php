a11cff4917585cf1127af7cfdf96ad8cf70aab8d
DNA-styx <154759339+DNA-styx@users.noreply.github.com>
2024-10-09 11:08:39 +0100

    tidy

--- web/pages/game.php
+++ web/pages/game.php
@@ -142,7 +142,7 @@ For support and installation notes visit http://www.hlxcommunity.com
 	$servers = $db->fetch_row_set();
 	$db->free_result();
 ?>
-
+<!-- start game.php -->
 <?php display_page_title('Servers'); ?>
 <?php
 if ($total_kills > 0)
@@ -185,7 +185,7 @@ if ($total_kills > 0)
               <!-- Card -->
               <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
                 <div class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full dark:text-teal-100 dark:bg-teal-500">
-					<i class="fas fa-crosshairs"></i>                
+				&nbsp;<i class="fas fa-crosshairs"></i>&nbsp;                
 				</div>
                 <div>
                   <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">Total Headshots</p>
@@ -618,3 +618,4 @@ if ($g_options['show_server_load_image'] == 1) {
 	}
 */
 ?>
+<!-- end game.php -->
