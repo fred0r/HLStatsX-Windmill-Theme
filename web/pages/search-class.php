c6b2bfb13030691301b7b6cf63d9129eb5b49324
DNA-styx <154759339+DNA-styx@users.noreply.github.com>
2024-10-13 21:09:12 +0100

    tidy

--- web/pages/search-class.php
+++ web/pages/search-class.php
@@ -131,7 +131,7 @@ For support and installation notes visit http://www.hlxcommunity.com
 							</td>
 						</tr>
 						<tr>
-							<td colspan="3">
+							<td colspan="2">
 								<input type="submit" value=" Find Now " class="windmill-button block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none">
 							</td> 
 						</tr>
