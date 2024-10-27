74d07c371506a25ee585b6a77d5bd1b49f5e8cd0
DNA-styx <154759339+DNA-styx@users.noreply.github.com>
2024-10-27 15:08:48 +0000

    Fix table colums

--- web/pages/livestats.php
+++ web/pages/livestats.php
@@ -526,8 +526,7 @@ function printserverstats($server_id)
 		{
 ?>
 			<tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-				<td>&nbsp;</td>
-				<td colspan="11">No Players</td>
+				<td colspan="11" style="text-align:center;">No Players</td>
 			</tr>
 			
 <?php
