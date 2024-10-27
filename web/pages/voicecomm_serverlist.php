b4d3e1e02dc5cb77f623385b40eb723a2c6af937
DNA-styx <154759339+DNA-styx@users.noreply.github.com>
2024-10-27 09:14:52 +0000

    Use new ts3admin query class

--- web/pages/voicecomm_serverlist.php
+++ web/pages/voicecomm_serverlist.php
@@ -23,18 +23,18 @@
 	if ($db->num_rows($result) >= 1) {
 ?>
 
+<!-- start voicecomm_serverlist.php -->
 <div class="w-full mt-8 overflow-hidden rounded-lg shadow-xs">
 	<div class="w-full overflow-x-auto">
-
-
 		<table class="w-full whitespace-no-wrap">
 			<tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-				<td class="fSmall">Voice Server Name</td>
-				<td class="fSmall">Server Address</td>
-				<td class="fSmall">Password</td>
-				<td class="fSmall" style="text-align:right;">Channels</td>
-				<td class="fSmall" style="text-align:right;">Slots&nbsp;used</td>
-				<td class="fSmall">Notes</td>
+				<td>Voice Server Name</td>
+				<td>Status</td>
+				<td>Server Address</td>
+				<td>Password</td>
+				<td>Slots&nbsp;used</td>
+				<td>Server Uptime</td>
+				<td>Notes</td>
 			</tr> 
 			<tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
 <?php
@@ -153,57 +153,97 @@
 					<?php echo $vent_server['descr']; ?>
 				</td>
 			</tr>
-<?php
+		<?php
 			}
 		}
 		
 				if (isset($ts3_servers))
 		{
-		  require_once(PAGE_PATH . '/teamspeak3_query.php');
-			foreach($ts3_servers as $ts3_server)
-			{
-			$tsstatus = new TSStatus($ts3_server['addr'], $ts3_server['queryPort'], $ts3_server['UDPPort']);
-			
-			$tsstatus->imagePath = IMAGE_PATH."/teamspeak3/";
-			$tsstatus->showNicknameBox = false;
-			$tsstatus->decodeUTF8 = false;
-			$tsstatus->timeout = 2;
-			//echo $tsstatus->render(); 
+			require_once(PAGE_PATH . '/ts3admin_class.php');
+			require_once('includes/inc_ts3admin_query_password.php');
 
-		?>  
-			<tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-				<td class="fHeading">
-					<img src="<?php echo IMAGE_PATH; ?>/teamspeak3/ts3.png" alt="tsicon" />
-					&nbsp;<a href="<?php echo $g_options['scripturl'] . "?mode=teamspeak&amp;game=$game&amp;tsId=".$ts3_server['serverId']; ?>"><?php echo $tsstatus->ts3name(); ?></a>
+		/**
+			*
+			* Is a small script to demonstrate how to get a serverlist via ts3admin.class
+			*
+			* by par0noid solutions - ts3admin.info https://github.com/Speckmops/ts3admin.class
+			*
+		*/
+
+		foreach($ts3_servers as $ts3_server)
+		{
+					
+					$ts3_ip = $ts3_server['addr'];
+					$ts3_queryport = $ts3_server['queryPort'];
+					$ts3_user = 'serveradmin';
+					$ts3_pass = $ts3q_server_query_password;
+					
+					#build a new ts3admin object
+					$tsAdmin = new ts3admin($ts3_ip, $ts3_queryport);
+					
+					if($tsAdmin->getElement('success', $tsAdmin->connect())) {
+						#login as serveradmin
+						$tsAdmin->login($ts3_user, $ts3_pass);
+						
+						#get serverlist
+						$servers = $tsAdmin->serverList();
+						
+						#generate table codes for all servers
+						foreach($servers['data'] as $server) {
+
+							$ts3q_server_id = $server['virtualserver_id'];
+							$ts3q_server_name = htmlspecialchars($server['virtualserver_name']);
+							$ts3q_server_port = $server['virtualserver_port'];
+							if(isset($server['virtualserver_clientsonline'])) {
+								$ts3q_server_clients = $server['virtualserver_clientsonline'] . '/' . $server['virtualserver_maxclients'];
+							}else{
+								$ts3q_server_clients = '-';
+							}
+							$ts3q_server_status = $server['virtualserver_status'];
+							if(isset($server['virtualserver_uptime'])) {
+								$ts3q_server_uptime = $tsAdmin->convertSecondsToStrTime(($server['virtualserver_uptime']));
+							}else{
+								$ts3q_server_uptime = '-';
+							}
+						}
+					}else{
+						$ts3q_server_id = '0';
+						$ts3q_server_name = '-';
+						$ts3q_server_port = '-';
+						$ts3q_server_clients = '-';
+						$ts3q_server_status = 'offline';
+						$ts3q_server_uptime = '-';
+					}
+
+		?>
+			<tr class="text-sm font-semibold text-gray-700 dark:text-gray-400">
+				<td class="flex items-center">
+					<img src="<?php echo IMAGE_PATH; ?>/teamspeak3/ts3.png" alt="tsicon">
+						&nbsp;<a href="<?php echo $g_options['scripturl'] . "?mode=teamspeak&amp;game=$game&amp;tsId=" . $ts3q_server_id; ?>">
+						<?php echo $ts3q_server_name ."\n"; ?>
+					</a>
 				</td>
+				<td><?php echo $ts3q_server_status ?></td>
 				<td>
-					<a href="ts3server://<?php echo $ts3_server['addr'].'?port='.$tsstatus->ts3_port() ?>&nickname=WebGuest">
-					<?php echo $ts3_server['addr'].':'.$tsstatus->ts3_port(); ?> (Join)
-					</a></td>
-				<td>
-					<?php echo $ts3_server['password']; ?>
+					<a href="ts3server://<?php echo $ts3_server['addr'].'?port='.$ts3q_server_port ?>&nickname=WebGuest">
+						<?php echo $ts3_server['addr'].':'.$ts3q_server_port; ?> (Join)
+					</a>
 				</td>
-				<td style="text-align:right;">
-					<?php echo $tsstatus->channelcount();  ?>
-				</td>
-				<td style="text-align:right;">
-					<?php echo $tsstatus->usage_chann(); ?>
-				</td>
-				<td>
-					<?php echo $ts3_server['descr']; ?>
-				</td>
+				<td>-</td>
+				<td><?php echo $ts3q_server_clients ?></td>
+				<td><?php echo $ts3q_server_uptime ?></td>
+				<td><?php echo $ts3_server['descr']; ?></td>
 			</tr>
-<?php
-     $tsstatus->disconn();
-			}
+<?php			
 		}
+}
 ?>
-		</tbody>
-	</table>
+			</tbody>
+		</table>
 	</div>
 <?php	echo "	<div class=\"rounded-b-lg border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800\">&nbsp;</div>\n"; ?>
 </div>
 <?php
 	}
-	// VOICECOMM MODULE END
 ?>
+<!-- end voicecomm_serverlist.php -->
