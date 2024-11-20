c9cd7c21d20a1410cd1bc6c0a462ee0b8a3286ab
DNA-styx <154759339+DNA-styx@users.noreply.github.com>
2024-11-20 14:21:32 +0000

    🦄 refactor: Change default TS3 Icon set

--- web/includes/teamspeak3/inc_ts3_settings.php
+++ web/includes/teamspeak3/inc_ts3_settings.php
@@ -8,11 +8,11 @@ $ts3_server_query_password = "server-query-password";
 /* Select the icon pack you want to use. e.g. 16x16, purple, ombregreen */
 /* Icon packs are located in hlstatsimg/teamspeak3/                     */
 
-$ts3_icon_pack = "16x16";
+$ts3_icon_pack = "purple";
 
 /* Cache Server Query results for X seconds. Set to 0 to disable        */
 /* Make sure web server has write permissions (777) to ts3ssv.php.cache */
 
-$ts3_cache_timeout = "0";
+$ts3_cache_timeout = "1";
 
 ?>
