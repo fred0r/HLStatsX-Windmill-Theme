8d080bc4b532e9f1d2288d7d2f7a92c03c5e8002
DNA-styx <154759339+DNA-styx@users.noreply.github.com>
2025-01-07 21:33:31 +0000

    🌈 style:Testing monospaced font

--- web/pages/header.php
+++ web/pages/header.php
@@ -145,10 +145,18 @@ include INCLUDE_PATH . '/inc_windmill_functions.php';
 	<meta name="robots" content="<?php echo $robot_meta_tag ?>">
 
 	<link rel="SHORTCUT ICON" href="favicon.ico">
+<!--
 	<link
       href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
       rel="stylesheet">
-    <link rel="stylesheet" href="./assets/css/tailwind.output.css">
+-->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+
+
+	<link rel="stylesheet" href="./assets/css/tailwind.output.css">
 
 	<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
   	<script src="./assets/js/fontawesome-all.min.js"></script>
