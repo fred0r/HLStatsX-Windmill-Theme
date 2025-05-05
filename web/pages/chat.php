c76a74267b77930a6a31af2592dc8ad2f5b8282f
DNA-styx <154759339+DNA-styx@users.noreply.github.com>
2025-05-05 21:29:26 +0100

    🐞 fix: Force log in to view chat https://github.com/NiDE-gg/hlstatsx-community-edition/pull/9/commits/634cfbe5d1d90b70b3659d65e26854830e938be4

--- web/pages/chat.php
+++ web/pages/chat.php
@@ -36,6 +36,194 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 For support and installation notes visit http://www.hlxcommunity.com
 */
 
+class Auth
+{
+	var $ok = false;
+	var $error = false;
+
+	var $username, $password, $savepass;
+	var $sessionStart, $session;
+
+	var $userdata = array();
+
+	function __construct()
+	{
+		//@session_start();
+
+		if (valid_request($_POST['authusername'], false))
+		{
+			$this->username = valid_request($_POST['authusername'], false);
+			$this->password = valid_request($_POST['authpassword'], false);
+			$this->savepass = valid_request($_POST['authsavepass'], false);
+			$this->sessionStart = 0;
+
+			# clear POST vars so as not to confuse the receiving page
+			unset($_POST);
+			$_POST = array();
+
+			$this->session = false;
+
+			if($this->checkPass()==true)
+			{
+				// if we have success, save it in this users SESSION
+				$_SESSION['username']=$this->username;
+				$_SESSION['password']=$this->password;
+				$_SESSION['authsessionStart']=time();
+				$_SESSION['acclevel'] = $this->userdata['acclevel'];
+			}
+		}
+		elseif (isset($_SESSION['loggedin']))
+		{
+			$this->username = $_SESSION['username'];
+			$this->password = $_SESSION['password'];
+			$this->savepass = 0;
+			$this->sessionStart = $_SESSION['authsessionStart'];
+			$this->ok = true;
+			$this->error = false;
+			$this->session = true;
+			
+			if(!$this->checkPass())
+			{
+				unset($_SESSION['loggedin']);
+			}
+		}
+		else
+		{
+			$this->ok = false;
+			$this->error = false;
+
+			$this->session = false;
+
+			$this->printAuth();
+		}
+	}
+
+	function checkPass()
+	{
+		global $db;
+
+		$db->query("
+				SELECT
+					*
+				FROM
+					hlstats_Users
+				WHERE
+					username='$this->username'
+				LIMIT 1
+			");
+
+		if ($db->num_rows() == 1)
+		{
+			// The username is OK
+
+			$this->userdata = $db->fetch_array();
+			$db->free_result();
+
+			if (md5($this->password) == $this->userdata["password"])
+			{
+				// The username and the password are OK
+
+				$this->ok = true;
+				$this->error = false;
+				$_SESSION['loggedin']=1;
+				if ($this->sessionStart > (time() - 3600))
+				{
+					// Valid session, update session time & display the page
+					$this->doCookies();
+					return true;
+				}
+				elseif ($this->sessionStart)
+				{
+					// A session exists but has expired
+					if ($this->savepass)
+					{
+						// They selected 'Save my password' so we just
+						// generate a new session and show the page.
+						$this->doCookies();
+						return true;
+					}
+					else
+					{
+						$this->ok = false;
+						$this->error = 'Your session has expired. Please try again.';
+						$this->password = '';
+
+						$this->printAuth();
+						return false;
+					}
+				}
+				elseif (!$this->session)
+				{
+					// No session and no cookies, but the user/pass was
+					// POSTed, so we generate cookies.
+					$this->doCookies();
+					return true;
+				}
+				else
+				{
+					// No session, user/pass from a cookie, so we force auth
+					$this->printAuth();
+					return false;
+				}
+			}
+			else
+			{
+				// The username is OK but the password is wrong
+
+				$this->ok = false;
+				if ($this->session)
+				{
+					// Cookie without 'Save my password' - not an error
+					$this->error = false;
+				}
+				else
+				{
+					$this->error = 'The password you supplied is incorrect.';
+				}
+				$this->password = '';
+				$this->printAuth();
+			}
+		}
+		else
+		{
+			// The username is wrong
+			$this->ok = false;
+			$this->error = 'The username you supplied is not valid.';
+			$this->printAuth();
+		}
+	}
+
+	function doCookies()
+	{
+		return;
+		setcookie('authusername', $this->username, time() + 31536000, '', '', 0);
+
+		if ($this->savepass)
+		{
+			setcookie('authpassword', $this->password, time() + 31536000, '', '', 0);
+		}
+		else
+		{
+			setcookie('authpassword', $this->password, 0, '', '', 0);
+		}
+		setcookie('authsavepass', $this->savepass, time() + 31536000, '', '', 0);
+		setcookie('authsessionStart', time(), 0, '', '', 0);
+	}
+
+	function printAuth()
+	{
+		global $g_options;
+
+		include (PAGE_PATH . '/adminauth.php');
+	}
+}
+
+	$auth = new Auth;
+	if ($auth->ok === false || $auth->userdata['acclevel'] < 80)
+	{
+		return;
+	}
+
     if (!defined('IN_HLSTATS')) {
         die('Do not access this file directly.');
     }
@@ -102,78 +290,58 @@ For support and installation notes visit http://www.hlxcommunity.com
 
 	<div class="flex items-center justify-center p-4 mb-8 text-sm px-4 py-3 bg-white rounded-lg shadow-md dark:bg-gray-800">
 
-			<form method="get" action="<?php echo $g_options['scripturl']; ?>">
-				<input type="hidden" name="mode" value="chat">
-				<input type="hidden" name="game" value="<?php echo $game; ?>">
-				<span class="font-semibold text-center text-gray-700 dark:text-gray-400">Show Chat from
-				<?php
-/*
-					$result = $db->query
-					("
-						SELECT
-							DISTINCT hlstats_Events_Chat.serverId,
-							hlstats_Servers.name
-						FROM
-							hlstats_Events_Chat
-						INNER JOIN
-							hlstats_Servers
-						ON
-							hlstats_Events_Chat.serverId = hlstats_Servers.serverId
-							AND hlstats_Servers.game='$game'
-						ORDER BY
-							hlstats_Servers.sortorder,
-							hlstats_Servers.name,
-							hlstats_Events_Chat.serverId ASC
-						LIMIT
-							0,
-							50
-					");
-*/
+		<form method="get" action="<?php echo $g_options['scripturl']; ?>">
+			<input type="hidden" name="mode" value="chat">
+			<input type="hidden" name="game" value="<?php echo $game; ?>">
+			<span class="font-semibold text-center text-gray-700 dark:text-gray-400">Show Chat from
+			<?php
 
-					$result = $db->query
-					("
-						SELECT
-							hlstats_Servers.serverId,
-							hlstats_Servers.name
-						FROM
-							hlstats_Servers
-						WHERE
-							hlstats_Servers.game='$game'
-						ORDER BY
-							hlstats_Servers.sortorder,
-							hlstats_Servers.name,
-							hlstats_Servers.serverId ASC
-						LIMIT
-							0,
-							50
-					");
 
-					echo '</span>';
-					echo '<select class="mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray" name="server_id"><option value="0">All Servers</option>';
-					$dates = array ();
-					$serverids = array();
-					while ($rowdata = $db->fetch_array())
-					{
-						$serverids[] = $rowdata['serverId'];
-						$dates[] = $rowdata; 
-						if ($showserver == $rowdata['serverId'])
-							echo '<option value="'.$rowdata['serverId'].'" selected>'.$rowdata['name'].'</option>';
-						else
-							echo '<option value="'.$rowdata['serverId'].'">'.$rowdata['name'].'</option>';
-					}
-					echo '</select>';
-					$filter=isset($_REQUEST['filter'])?$_REQUEST['filter']:"";
-				?>
-				<span class="font-semibold text-center text-gray-700 dark:text-gray-400">Filter: </span>
-				<input type="text" name="filter" value="<?php echo htmlentities($filter); ?>" class="mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"> 
-				<input type="submit" value="View" class="windmill-button px-4 py-2 mb-2 text-sm font-medium leading-5 text-center border border-transparent rounded-lg btn">
-			</form>
-		</div>
+				$result = $db->query
+				("
+					SELECT
+						hlstats_Servers.serverId,
+						hlstats_Servers.name
+					FROM
+						hlstats_Servers
+					WHERE
+						hlstats_Servers.game='$game'
+					ORDER BY
+						hlstats_Servers.sortorder,
+						hlstats_Servers.name,
+						hlstats_Servers.serverId ASC
+					LIMIT
+						0,
+						50
+				");
 
-	
-<!-- start table -->		
+				echo '</span>';
+				echo '<select class="mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray" name="server_id"><option value="0">All Servers</option>';
+				$dates = array ();
+				$serverids = array();
+				while ($rowdata = $db->fetch_array())
+				{
+					$serverids[] = $rowdata['serverId'];
+					$dates[] = $rowdata; 
+					if ($showserver == $rowdata['serverId'])
+						echo '<option value="'.$rowdata['serverId'].'" selected>'.$rowdata['name'].'</option>';
+					else
+						echo '<option value="'.$rowdata['serverId'].'">'.$rowdata['name'].'</option>';
+				}
+				echo '</select>';
+				$filter=isset($_REQUEST['filter'])?$_REQUEST['filter']:"";
+			?>
+			<span class="font-semibold text-center text-gray-700 dark:text-gray-400">Filter: </span>
+			<input type="text" name="filter" value="<?php echo htmlentities($filter); ?>" class="mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"> 
+			<input type="submit" value="View" class="windmill-button px-4 py-2 mb-2 text-sm font-medium leading-5 text-center border border-transparent rounded-lg btn">
+		</form>
+	</div>
 
-	<?php
+
+
+
+	<div style="clear:both;padding-top:20px;"></div>
+		<?php
 			if ($showserver == 0)
 			{
 				$table = new Table(
@@ -295,15 +463,6 @@ For support and installation notes visit http://www.hlxcommunity.com
 					$table->startitem,
 					$table->numperpage;
 			", true, false);
-/*
-    			$whereclause = "hlstats_Events_Chat.serverId ";
-
-			if($showserver == 0) {
-				$whereclause .= "in (".implode($serverids,',').")";
-			} else {
-				$whereclause .= "= $showserver";
-			}
-*/
 
 			$db->query
 			("
@@ -331,7 +490,5 @@ For support and installation notes visit http://www.hlxcommunity.com
 
 			$table->draw($result, $numitems, 95);
 		?>
-
-<!-- end table -->
-
+	</div>
 <!-- end chat.php -->
